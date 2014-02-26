3d1ab66721369eafaf384294547e14a03daa88d1
Henryk Konsek <hekonsek@gmail.com>
2014-02-26 13:19:34 +0100

    Refactoring.

--- micromix-infra/micromix-infra-restgateway/micromix-infra-restgateway-spring/src/main/scala/micromix/services/restgateway/spring/DefaultGatewayRequest.scala
+++
@@ -1,3 +0,0 @@
-package micromix.services.restgateway.spring
-
-case class DefaultGatewayRequest(service: String, operation: String, parameters: Array[Any]) extends GatewayRequest
--- micromix-infra/micromix-infra-restgateway/micromix-infra-restgateway-spring/src/main/scala/micromix/services/restgateway/spring/GatewayRequestMapper.scala
+++
@@ -1,7 +0,0 @@
-package micromix.services.restgateway.spring
-
-trait GatewayRequestMapper[T <: Any] {
-
-  def mapRequest(rawRequest: T): GatewayRequest
-
-}
