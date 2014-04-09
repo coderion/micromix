d52604377002a34c695ff15653b71b77254d905b
Henryk Konsek <hekonsek@gmail.com>
2014-04-09 10:26:05 +0200

    Fixed properties placeholder file path.

--- micromix-boots/micromix-boot-spring/src/main/java/micromix/boot/spring/PropertiesPlaceholderConfiguration.java
+++ micromix-boots/micromix-boot-spring/src/main/java/micromix/boot/spring/PropertiesPlaceholderConfiguration.java
@@ -11,7 +11,7 @@ public class PropertiesPlaceholderConfiguration {
     @Bean
     static public PropertyPlaceholderConfigurer myPropertySourcesPlaceholderConfigurer() {
         PropertyPlaceholderConfigurer propertiesConfigurer = new PropertyPlaceholderConfigurer();
-        propertiesConfigurer.setLocation(new ClassPathResource("/micromix.boot.spring/property-placeholder.xml"));
+        propertiesConfigurer.setLocation(new ClassPathResource("/micromix/boot/spring/micromix.properties"));
         propertiesConfigurer.setIgnoreResourceNotFound(true);
         propertiesConfigurer.setSystemPropertiesMode(PropertyPlaceholderConfigurer.SYSTEM_PROPERTIES_MODE_OVERRIDE);
         return propertiesConfigurer;
