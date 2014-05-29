55cd55909b27df387518b2442b5822b73279376d
Henryk Konsek <hekonsek@gmail.com>
2014-05-29 08:33:26 +0200

    Migrated interceptors to Java.

--- micromix-conflets/micromix-conflet-restgateway/micromix-conflet-restgateway-spring/src/main/scala/micromix/services/restgateway/spring/DefaultGatewayRequest.scala
+++
@@ -1,6 +0,0 @@
-package micromix.services.restgateway.spring
-
-import micromix.services.restgateway.api.GatewayRequest
-import java.util.{Map => JMap}
-
-case class DefaultGatewayRequest(headers: JMap[String, String], service: String, operation: String, parameters: Array[String]) extends GatewayRequest
